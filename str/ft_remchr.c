bf1bf38b6c35bfaf5a6e0b30776f0c2e7812b3ac
TL_Potatoe <titouan.lamit@gmail.com>
2026-04-02 14:49:00 +0200

    fixed an issue with ft_remchr when it would break when using negative char

--- str/ft_remchr.c
+++ str/ft_remchr.c
@@ -6,15 +6,22 @@
 /*   By: tlamit <titouan.lamit@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 16:49:00 by tlamit            #+#    #+#             */
-/*   Updated: 2026/04/01 16:46:09 by tlamit           ###   ########.fr       */
+/*   Updated: 2026/04/02 16:13:26 by tlamit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_str.h"
 
+static size_t always_pos(int i)
+{
+	if (i < 0)
+		i = (i*-1+256);
+	return (i);
+}
+
 void	ft_remchr(char *s, char *char_list)
 {
-	char	seen[256];
+	char	seen[512];
 	int		i;
 	int		j;
 
@@ -22,12 +29,12 @@ void	ft_remchr(char *s, char *char_list)
 		return ;
 	ft_memset(seen, 0, sizeof seen);
 	while (*char_list)
-		seen[(int)*(char_list++)] = 1;
+		seen[always_pos((int)*(char_list++))] = 1;
 	i = -1;
 	j = 0;
 	while (s[++i])
 	{
-		if (seen[(int)s[i]])
+		if (seen[always_pos((int)s[i])])
 			continue ;
 		s[j++] = s[i];
 	}
